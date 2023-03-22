f7e83e1134518e2cf499603ab44354869b1968bf
mozammil <mozammil.raja@antiersolutions.com>
2023-03-22 09:40:39 +0530

    wallet connect

---
+++ src/HomePage.js
@@ -0,0 +1,10 @@
+import { Web3Button, Web3NetworkSwitch } from '@web3modal/react'
+import CustomButton from '../components/CustomButton'
+
+export default function HomePage() {
+    return (
+        <div>
+            <CustomButton />
+        </div>
+    )
+}
